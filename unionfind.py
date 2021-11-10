9cbeb289b7eebb9ae941ea7e2bea12e50fd7d57a
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-10 16:41:49 -0500

    simplified unionfind

--- unionfind.py
+++ unionfind.py
@@ -2,10 +2,8 @@
 Union-Find is a data structure that tracks and merges "disjoint sets". Union-
 Find operations include:
 
-1) "count" number of disjoint components;
-2) "connect" two disjoint components into one; 
-3) "find" which subset an element is in; 
-4) "union" two subsets into one
+1) "find" which subset an element is in; 
+2) "union" two subsets into one
 
                      algorithm | worst-case time |
 -------------------------------------------------|           
@@ -30,134 +28,45 @@ class UnionFind:
 
 	UnionFind (aka Quick Union) is a "lazy" approach.
 
-	In addition, two 
-	techniques are used to improve performance: 
-	* weighting, which links root of smaller tree to root of larger tree;
-	* path compression, which updates parent to root while search for it.
+	In addition, two techniques are used to improve performance: 
+	1) find with path compression, which updates parent to root while search for it.
+	2) uion with rank, which links smaller tree to larger tree;
 	"""
 
-	def __init__(self, N: int):
-		"""Initialize a Quick-Union object ~ O(N)
-		In Quick-Union, connected components are reflected in common root. 
+	def __init__(self, n: int):
+		"""Initialize a UnionFind object ~ O(N)
+		In UnionFind, connected components are reflected in common roots. 
 		"""
-		self.count = N               # count of disjoint components
-		self.parent = list(range(N)) # parent array (to reflect subsets)
-		self.rank = [1] * N          # size of subtree
-
-	def __len__(self):
-		"""Return number of nodes (not number of disjoint components)."""
-		return len(self.parent)
-
-	def __repr__(self):
-		"""Return string representation of Quick-Union."""
-		return str(self.parent)
-
-	def connected(self, p: int, q: int) -> bool:
-		"""Return True if p & q are connected ~ O(1) on average."""
-		return self.find(p) == self.find(q)
-
-	def count(self) -> int:
-		"""Return the number of disjoint components ~ O(1)."""
-		return self.count
+		self.parent = list(range(n)) # parent array (to reflect subsets)
+		self.rank = [1] * n          # size of subtree
 
 	def find(self, p: int, halving: bool=True) -> int:
-		"""Return the root of object at p with path compression ~ O(1) on 
-		average.
-		"""
+		"""Find with path compression ~ O(1) on average."""
 		if p != self.parent[p]:
 			self.parent[p] = self.find(self.parent[p]) # path compression
 		return self.parent[p]
 
-	def union(self, p: int, q: int, ranking: bool=True) -> None:
-		"""Connect p & q ~ O(1) on average
-
-		Arguments:
-		ranking -- a flag to turn on tree ranking
-
-		If ranking is on, the smaller tree is always linked to the larger 
-		tree to achieve better balance. If ranking is off, p-tree is linked 
-		to q-tree (i.e. set parent of p-root to q-root).
-		"""
+	def union(self, p: int, q: int) -> bool:
+		"""Union with rank ~ O(1) on average"""
 		prt, qrt = self.find(p), self.find(q)
-		if prt == qrt: return #already linked
-		
-		if ranking and self.rank[prt] > self.rank[qrt]: 
-			prt, qrt = qrt, prt #p-tree is smaller 
-		self.count -= 1
-		self.parent[prt] = qrt #link p-tree to q-tree
-		self.rank[qrt] += self.rank[prt] #update q-tree size
-
-
-class QuickFind:
-	"""Quick-Find abstracts union-find problem into an identity array, in 
-	which two objects, p & q, are connected iff their entries in the array 
-	are the same.
-
-	Quick-Find is a "gready" approach. 
-	"""
-
-	def __init__(self, N: int):
-		"""Initialize a Quick-Find object ~ O(N)
-		In Quick-Find, connected components are reflected in common id. 
-		"""
-		self.count = N
-		self.id = list(range(N)) #identity array (to reflect membership)
-
-	def __len__(self):
-		"""Return number of nodes (not number of disjoint components)"""
-		return len(self.id)
-
-	def __repr__(self):
-		"""Return string representation of Quick-Find"""
-		return str(self.id)
-
-	def connected(self, p: int, q: int) -> bool:
-		"""Return True if p & q are connected ~ O(N)"""
-		return self.id[p] == self.id[q]
-
-	def count(self) -> int:
-		"""Return the number of disjoint components ~ O(1)"""
-		return self.count 
-
-	def find(self, p: int) -> int:
-		"""Return the membership of p ~ O(1)"""
-		return self.id[p]
-
-	def union(self, p: int, q: int) -> None:
-		"""Connect p & q by setting id of p component to that of q ~ O(N)"""
-		pid, qid = self.id[p], self.id[q]
-		if pid == qid: return #already linked
-		self.count -= 1
-		for i, iid in enumerate(self.id):
-			if iid == pid: self.id[i] = qid
+		if prt == qrt: return False # already linked
+		if self.rank[prt] > self.rank[qrt]: prt, qrt = qrt, prt # rank[prt] <= rank[qrt]
+		self.parent[prt] = qrt
+		self.rank[qrt] += self.rank[prt]
+		return True
 
 
 if __name__ == "__main__": 
-	"""Test client for QuickFind"""
-	uf = QuickFind(10)
-	uf.union(4, 3)
-	uf.union(3, 8)
-	uf.union(6, 5)
-	uf.union(9, 4)
-	uf.union(2, 1)
-	print("8-9? ", uf.connected(8, 9)) #true
-	print("5-0? ", uf.connected(5, 0)) #false
-	uf.union(5, 0)
-	uf.union(7, 2)
-	uf.union(6, 1)
-	print("5-0? ", uf.connected(5, 0)) #true
-
-
-	"""Test client for QuickUnion"""
+	"""Test client for UnionFind"""
 	uf = UnionFind(10)
 	uf.union(4, 3)
 	uf.union(3, 8)
 	uf.union(6, 5)
 	uf.union(9, 4)
 	uf.union(2, 1)
-	print("8-9? ", uf.connected(8, 9)) #true
-	print("5-0? ", uf.connected(5, 0)) #false
+	print("8-9? ", uf.find(8) == uf.find(9)) # true
+	print("5-0? ", uf.find(5) == uf.find(0)) # false
 	uf.union(5, 0)
 	uf.union(7, 2)
 	uf.union(6, 1)
-	print("5-0? ", uf.connected(5, 0)) #true
+	print("5-0? ", uf.find(5) == uf.find(0)) # true
